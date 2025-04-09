79cc1fb7fbd7a1870ac1264473ef8ac48efa6110
elainelima <elainebarbosalimaa@gmail.com>
2025-04-09 20:03:26 -0300

    finalizando alterações

--- C++/PA1/src/evalenum.cpp
+++ C++/PA1/src/evalenum.cpp
@@ -76,23 +76,21 @@ void swap(int *xp, int *yp, sortperf_t *s) {
 
 // shellsort
 void shellSort(int *A, int n, sortperf_t * s) {
-    if (s) {
-        s->cmp = 0;
-        s->move = 0;
-    }
-
+    inccalls(s,1);
     for (int gap = n / 2; gap > 0; gap /= 2) {
         for (int i = gap; i < n; i++) {
             int temp = A[i];
             int j;
-
+            incmove(s, 1);
+            inccmp(s,1);
             for (j = i; j >= gap && A[j - gap] > temp; j -= gap) {
                 A[j] = A[j - gap];
-                if (s) s->cmp++;
+                inccmp(s,1);
+                incmove(s, 1);
             }
 
             A[j] = temp;
-            if (s && j != i) s->move++;
+            incmove(s, 1);
         }
     }
 
@@ -109,7 +107,7 @@ void recursiveSelectionSort(int arr[], int l, int r, sortperf_t * s)
     for (int j = l + 1; j <= r; j++)
     {
         // if `arr[j]` is less, then it is the new minimum
-	inccmp(s,1);
+	    inccmp(s,1);
         if (arr[j] < arr[min]) {
             min = j;    // update the index of minimum element
         }
@@ -158,31 +156,24 @@ void insertionSort(int v[], int l, int r, sortperf_t * s) {
     inccalls(s,1);
     for (i = l + 1; i <= r; i++) {
         key = v[i];
+        incmove(s, 1);
         j = i - 1;
-        
-        // Compare key with each element on the left until a smaller element is found
-        // or we reach the left boundary
+        inccmp(s,1);
         while (j >= l && v[j] > key) {
-            s->cmp++; // Count comparison
-            v[j + 1] = v[j]; // Move element one position to the right
-            s->move++; // Count movement
+            inccmp(s,1); 
+            v[j+1] = v[j];
+            incmove(s, 1);
             j--;
         }
-        
-        // If j >= l, we need one more comparison that resulted in exiting the loop
-        if (j >= l) s->cmp++;
-        
-        // Place the key in its correct position
-        v[j + 1] = key;
-        s->move++; // Count movement for placing key
+        v[j+1] = key;
+        incmove(s, 1);
     }
-    
-    return;
+    //return;
 }
 
 // median of 3 integers
 int median (int a, int b, int c) {
-    if ((a <= b) && (b <= c)) return b;  // a b c
+    if ((a <= b) && (b <= c)) return b;  // a b 
     if ((a <= c) && (c <= b)) return c;  // a c b
     if ((b <= a) && (a <= c)) return a;  // b a c
     if ((b <= c) && (c <= a)) return c;  // b c a
@@ -191,76 +182,34 @@ int median (int a, int b, int c) {
 }
 
 void partition3(int *A, int l, int r, int *i, int *j, sortperf_t *s) {
-    // Handle base case
-    if (r <= l) {
-        *i = l;
-        *j = r;
-        return;
-    }
-    
-    // Find median of three and use it as pivot
-    int mid = l + (r - l) / 2;
-    int pivot_idx = median(mid, l, r);
-    int pivot = A[pivot_idx];
-    
-    // Move pivot to the beginning
-    swap(&A[l], &A[pivot_idx], s);
-    
-    
-    // Initialize pointers for three-way partition
-    int lt = l;      // Less than pivot (left boundary)
-    int gt = r;      // Greater than pivot (right boundary)
-    int i_ptr = l+1; // Current element being examined
-    
-    // Partition the array
-    while (i_ptr <= gt) {
-        // Compare current element with pivot
-        s->cmp++;
-        
-        if (A[i_ptr] < pivot) {
-            // Element is less than pivot
-            swap(&A[lt], &A[i_ptr], s);
-            lt++;
-            i_ptr++;
-        } else if (A[i_ptr] > pivot) {
-            // Element is greater than pivot
-            swap(&A[i_ptr], &A[gt], s);
-            gt--;
-        } else {
-            // Element is equal to pivot
-            i_ptr++;
-        }
-    }
-    // Update output parameters to mark partitioning boundaries
-    *i = lt;   // Left boundary (first element == pivot)
-    *j = gt;   // Right boundary (last element == pivot)
-}
-
-
-
-// standard quicksort partition
-void partition(int * A, int l, int r, int *i, int *j, sortperf_t *s) {
-    int pivot, aux;
     *i = l;
     *j = r;
+    int mid =  A[(*i + *j)/2];
+    int left = A[*j];
+    int right = A[*i];
     
+    inccalls(s,1);
+    int pivot_idx = median(mid, left, right);
+    int pivot =  pivot_idx;
     // Choose the pivot as the middle element
-    pivot = A[(*i + *j)/2];
-    
+   
     do {
+        inccmp(s, 1);
         // Find element on left side that is greater than pivot
         while (A[*i] < pivot) {
             (*i)++;
-            s->cmp++;
+            inccmp(s, 1);
         }
         
         // Find element on right side that is less than pivot
         while (A[*j] > pivot) {
             (*j)--;
-            s->cmp++;
+            inccmp(s, 1);
+
         }
         
         // If we found a pair of elements to swap
+        inccmp(s, 1);
         if (*i <= *j) {
             // Swap elements
             swap(&A[*i], &A[*j], s);
@@ -268,91 +217,115 @@ void partition(int * A, int l, int r, int *i, int *j, sortperf_t *s) {
             (*j)--;
         }
     } while (*i <= *j);
+    
+}
+
+
+
+// standard quicksort partition
+void partition(int * A, int l, int r, int *i, int *j, sortperf_t *s) {
+    int pivot;
+    *i = l;
+    *j = r;
+    inccalls(s,1);
+    // Choose the pivot as the middle element
+    pivot = A[(*i + *j)/2];
+   
+    do {
+        inccmp(s, 1);
+        // Find element on left side that is greater than pivot
+        while (A[*i] < pivot) {
+            (*i)++;
+            inccmp(s, 1);
+        }
+        
+        // Find element on right side that is less than pivot
+        while (A[*j] > pivot) {
+            (*j)--;
+            inccmp(s, 1);
+
+        }
+        
+        // If we found a pair of elements to swap
+        inccmp(s, 1);
+        if (*i <= *j) {
+            // Swap elements
+            swap(&A[*i], &A[*j], s);
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+    
 }
 
 // Implementação do QuickSort padrão
 void quickSort(int *A, int l, int r, sortperf_t *s) {
-    if (l < r) {
-        int i, j;
-        
-        // Particiona o array e obtém os índices i e j
-        partition(A, l, r, &i, &j, s);
-        
-        // Ordena recursivamente as subpartes
-        // Parte esquerda (elementos menores que o pivô)
+    int i, j;
+    inccalls(s,1);
+    partition(A, l, r, &i, &j, s);
+    if (l < j ) {
         quickSort(A, l, j, s);
-        
-        // Parte direita (elementos maiores que o pivô)
+    }
+    //inccmp(s, 1);
+    if(i < r) {
         quickSort(A, i, r, s);
     }
-}
+}   
 
 // quicksort with median of 3
 void quickSort3(int * A, int l, int r, sortperf_t *s) { 
-    if (l < r) {
-        int i, j;
-        
-        // Particiona o array e obtém os índices i e j
-        partition3(A, l, r, &i, &j, s);
-        
-        // Ordena recursivamente as subpartes
-        // Parte esquerda (elementos menores que o pivô)
-        quickSort(A, l, j, s);
-        
-        // Parte direita (elementos maiores que o pivô)
-        quickSort(A, i, r, s);
+    int i, j;
+    inccalls(s,1);
+    partition3(A, l, r, &i, &j, s);
+    if (l < j ) {
+        quickSort3(A, l, j, s);
     }
-}
+    if(i < r) {
+        quickSort3(A, i, r, s);
+    }
+}  
+
 
 // Quicksort with insertion sort for small partitions
 void quickSortIns(int * A, int l, int r, sortperf_t *s) {
     // Define threshold for small partitions
-    const int THRESHOLD = 10;
-    
-    // Base case: if the partition is smaller than threshold or invalid
-    if (l >= r) {
-        return;
-    }
-    
-    // For small partitions, use insertion sort
-    if (r - l + 1 <= THRESHOLD) {
-        insertionSort(A, l, r, s);
-        return;
-    }
-    
-    // For larger partitions, use partition3 and then recurse
+    inccalls(s, 1);
     int i, j;
-    
-    // Partition the array
     partition(A, l, r, &i, &j, s);
+ 
+    if (l < j) {
+        if (j-l  <= 50) {
+            insertionSort(A, l, j, s);
+        }
+        else  quickSortIns(A, l, j, s);
+    }
+    if (i < r) {
+       if (r-i  <= 50) {
+            insertionSort(A, i, r, s);
+        }
+        else  quickSortIns(A, i, r, s);
+    }
     
-    // Recursively sort the partitions
-    quickSortIns(A, l, j, s);
 }
 // quicksort with insertion for small partitions and median of 3
 void quickSort3Ins(int * A, int l, int r, sortperf_t *s) { 
     // Define threshold for small partitions
-    const int THRESHOLD = 10;
-    
-    // Base case: if the partition is smaller than threshold or invalid
-    if (l >= r) {
-        return;
-    }
-    
-    // For small partitions, use insertion sort
-    if (r - l + 1 <= THRESHOLD) {
-        insertionSort(A, l, r, s);
-        return;
-    }
-    
-    // For larger partitions, use partition3 and then recurse
+    inccalls(s, 1);
     int i, j;
-    
-    // Partition the array
     partition3(A, l, r, &i, &j, s);
-    
-    // Recursively sort the partitions
-    quickSortIns(A, l, j, s);
+ 
+    if (l < j) {
+        if (j-l  <= 50) {
+            insertionSort(A, l, j, s);
+        }
+        else  quickSort3Ins(A, l, j, s);
+    }
+    if (i < r) {
+       if (r-i  <= 50) {
+            insertionSort(A, i, r, s);
+        }
+        else  quickSort3Ins(A, i, r, s);
+    }
 }
 void uso() {
     fprintf(stderr, "sortperf\n");
